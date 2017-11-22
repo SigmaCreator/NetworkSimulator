a95b07bafb9f61fc4e193999324f28fb6bea6196
SigmaCreator <mourarovane@gmail.com>
2017-11-22 17:28:14 -0200

    changed runner

--- src/network/simulator/Runner.java
+++ src/network/simulator/Runner.java
@@ -1,45 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package network.simulator;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-/**
- *
- * @author user
- */
 public class Runner {
     
     public static void main (String [] args)
     {
         NetworkSimulator net = new NetworkSimulator();
         
-        BufferedReader br = null;
-        
-        try 
-        {
-            br = new BufferedReader(new FileReader(args[0]));
-            
-            String line, topology, sender, recipient, message;
-            
-            while ((line = br.readLine()) != null)
-            {
-                String [] info = line.split(" ");
-                topology = info[0];
-                sender = info[1];
-                recipient = info[2];
-                message = info[3];
-                
-                net.boot(topology, sender, recipient, message);
-                
-            }
-        
-        } catch (IOException e) { e.printStackTrace(); } 
+        net.boot(args[0], args[1], args[2], args[3]);   
+
     }
     
 }
